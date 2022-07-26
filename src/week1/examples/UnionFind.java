661a7597394a5bbdd4b03a25728a031e37bf82fb
mx_in <mouxin1993@gmail.com>
2022-07-27 00:21:43 +0800

    feat: client

--- src/week1/examples/UnionFind.java
+++ src/week1/examples/UnionFind.java
@@ -1,7 +1,18 @@
 package week1.examples;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.UF;
+
 public class UnionFind {
     public static void main(string[] args) {
-
+        int N = StdIn.readInt();
+        UF uf = new UF(N);
+        while(!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
     }
 
     /**
