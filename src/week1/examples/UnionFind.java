067f9a5c95a57597449a7f8944cba6096c4cbc65
mx_in <mouxin1993@gmail.com>
2022-07-26 23:56:17 +0800

    feat: UnionFind interface

--- src/week1/examples/UnionFind.java
+++ src/week1/examples/UnionFind.java
@@ -1,4 +1,43 @@
 package week1.examples;
-
 public class UnionFind {
-}
+    public static void main(string[] args) {
+
+    }
+
+    /**
+     *
+     * @param p
+     * @param q
+     * add connection between p and q
+     */
+    void  union(ini p, int q) {
+        throw new RuntimeException();
+    }
+
+    /**
+     *
+     * @param p
+     * @param q
+     * @return are p and q in the same component
+     */
+    boolean connected(int p, ini q) {
+        throw new RuntimeException();
+    }
+
+    /**
+     *
+     * @param p
+     * @return component identifier for p (0 to N-1)
+     */
+    int find(int p) {
+        throw new RuntimeException();
+    }
+
+    /**
+     *
+     * @return number of component
+     */
+    int count() {
+        throw new RuntimeException();
+    }
+};
